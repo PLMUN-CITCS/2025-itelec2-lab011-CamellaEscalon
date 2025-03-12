c00d16e7f9b10ef22eddba21f54dd90150a12335
CamellaEscalon <>
2025-03-12 11:45:08 +0800

    Last Sync: 2025-03-12 03:45:08 (Mobile)

--- nested_if_else_statement.py
+++ nested_if_else_statement.py
@@ -1,17 +1,9 @@
-# Prompt user for their age and membership status
 age_str = input("Enter your age: ")
 membership_str = input("Are you a member? (Yes/No): ")
 
-# Convert age to an integer and normalize membership input
-try:
-    age = int(age_str)  # Convert age to integer
-except ValueError:
-    print("Invalid age input. Please enter an integer.")
-    exit()  # Exit the program if the input is not a valid integer
+age = int(age_str)
+membership = membership_str.strip().lower()
 
-membership = membership_str.strip().lower()  # Normalize membership input
-
-# Nested if-else statements to check conditions
 if age >= 18:
     if membership == "yes":
         print("Access granted.")
